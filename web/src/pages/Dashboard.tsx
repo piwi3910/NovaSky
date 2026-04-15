6bb72f6753727fba6c1ffd67b6e526d5e823236e
Pascal Watteel <pascal@watteel.com>
2026-04-15 21:55:45 +0400

    fix: center scroll position when opening fullscreen frame

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -49,6 +49,7 @@ export function Dashboard() {
 
       {fullscreen && frameId && hasJpeg && (
         <div
+          ref={(el) => { if (el) { el.scrollLeft = (el.scrollWidth - el.clientWidth) / 2; el.scrollTop = (el.scrollHeight - el.clientHeight) / 2; } }}
           onClick={() => setFullscreen(false)}
           style={{
             position: "fixed", top: 0, left: 0, right: 0, bottom: 0,
